af47bced1256d97ee69060f8fea0864d9d919c30
nsonidotdev <anton.stecyura.pr1@gmail.com>
2026-09-23 11:39:30 +0300

    feat(OTLP): map raw span status and reason to OTLP status

--- otlp.go
+++ otlp.go
@@ -14,15 +14,20 @@ func SpansToOTLPJSON(spans []*Span) (*otlp.ExportTraceServiceRequest, error) {
 	}
 	otlpSpans := make([]*otlp.Span, 0, len(spans))
 	for _, span := range spans {
+		if _, ok := span.Attributes["gotrail.skipped"]; !ok && span.status == statusSkip {
+			span.Attributes["gotrail.skipped"] = true
+		}
 		attributes := otlp.SerializeAttributes(span.Attributes)
+		status := toOTLPSpanStatus(span)
 		otlpSpan := &otlp.Span{
 			TraceID:           hex.EncodeToString(span.Trace.ID[:]),
 			SpanID:            hex.EncodeToString(span.ID[:]),
 			Name:              span.Name,
 			StartTimeUnixNano: strconv.FormatInt(span.Start.UnixNano(), 10),
 			EndTimeUnixNano:   strconv.FormatInt(span.Start.UnixNano()+int64(span.Duration), 10),
-			Attributes:        attributes,
 			Kind:              otlp.SpanKindServer,
+			Status:            status,
+			Attributes:        attributes,
 		}
 		if span.parent != nil {
 			otlpSpan.ParentSpanID = hex.EncodeToString(span.parent.ID[:])
@@ -65,4 +70,28 @@ func SpansToOTLPJSON(spans []*Span) (*otlp.ExportTraceServiceRequest, error) {
 	return &otlp.ExportTraceServiceRequest{
 		ResourceSpans: resourceSpans,
 	}, nil
+}
+
+func toOTLPSpanStatus(s *Span) *otlp.SpanStatus {
+	switch s.status {
+	case statusFail:
+		return &otlp.SpanStatus{
+			Code:    otlp.StatusError,
+			Message: s.reason,
+		}
+	case statusSuccess:
+		return &otlp.SpanStatus{
+			Code: otlp.StatusOK,
+		}
+
+	case statusSkip:
+		return &otlp.SpanStatus{
+			Code:    otlp.StatusOK,
+			Message: s.reason,
+		}
+	}
+
+	return &otlp.SpanStatus{
+		Code: otlp.StatusUnset,
+	}
 }

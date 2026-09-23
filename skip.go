dc9766680ecf7576b0bce6ae7618b9960a147eda
nsonidotdev <anton.stecyura.pr1@gmail.com>
2026-09-23 11:37:58 +0300

    fix: pass statusSkip to skip the span

--- skip.go
+++ skip.go
@@ -4,14 +4,14 @@ import "context"
 
 func Skip(ctx context.Context, reason string) {
 	end(ctx, endOptions{
-		status: statusSuccess,
+		status: statusSkip,
 		reason: reason,
 	})
 }
 
 func (s *Span) Skip(reason string) {
 	s.end(endOptions{
-		status: statusSuccess,
+		status: statusSkip,
 		reason: reason,
 	})
 }
